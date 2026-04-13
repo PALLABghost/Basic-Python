fcc926a42bff6b2f550ff608a725aeb3240d9e9a
Pallab <pallab.basak.924@gmail.com>
2026-04-13 21:57:14 +0530

    How to create funtion

---
+++ 15. create funtion.py
@@ -0,0 +1,9 @@
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " +last
+
+
+fullname = create_name("pallab","basak")
+
+print(fullname)
