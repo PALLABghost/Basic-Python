134bc9e402a6cf5f654d7586a2c895e1a630dd3a
Pallab <pallab.basak.924@gmail.com>
2026-04-16 01:09:24 +0530

    Class variable , instance variable

---
+++ student.py
@@ -0,0 +1,19 @@
+class Student:
+    class_year = 2024    # class variable ,shared among the all instances of a class
+    num_of_student = 0                #define outside of the constractor
+                        #allow the share data between all object created in the class
+
+    def __init__(self,name,age):     #__init__(self) is a constractor
+        self.name = name            #intance variable
+        self.age = age
+        Student.num_of_student += 1     #calling class variable and when a student(object) created increment the num
+
+student1 = Student("Suresh", 30)
+student2 = Student("Roma", 35)
+student3 = Student("Jesh", 37)
+
+print(student2.name)
+print(student2.age)
+print(Student.class_year)
+print(f"My Graduating class of {Student.class_year} has {Student.num_of_student} students")
+
