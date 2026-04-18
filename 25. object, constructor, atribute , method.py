7695d5a955009bab1a128f721465d897bce5245c
Pallab <pallab.basak.924@gmail.com>
2026-04-18 12:22:12 +0530

    Class define in car file

--- 25. object, constructor, atribute , method.py
+++ 25. object, constructor, atribute , method.py
@@ -1,6 +1,22 @@
 from car import Car
 
-car1 = Car("Mustang", 2024, "red" ,False)
+#class Car:
+ #   def __init__(self, model, year, color, for_sale):   #__init__(self) is a constractor
+ #      self.model = model
+ #      self.year = year
+ #      self.color = color
+  #     self.for_sale =for_sale
+
+   # def drive(self):
+   #     print(f"You drive the {self.color} {self.model}")
+
+  #  def stop(self):
+  #      print(f"You stop the  {self.color} {self.model}")
+
+  #  def describe(self):
+  #      print(f"{self.model} {self.color} {self.year}")}
+
+car1 = Car("Mustang", 2024, "red" ,False)       #object created in class
 car2 = Car("Audi", 2020, "white" ,True)
 print(car1.model)
 print(car2.year)
